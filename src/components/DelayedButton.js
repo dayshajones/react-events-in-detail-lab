9c01d8fe9eb66c9933aba29cdc9c9b98d43a8032
“Daysha <+>
2022-04-13 12:38:41 -0700

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,22 @@
-// Code DelayedButton Component Here
+import React, {Component} from 'react'
+
+class DelayedButton extends Component{
+
+   
+	setTimeout = (event) => {
+		event.persist()
+		this.props.delay
+		this.props.onDelayedClick(event)
+	}
+
+	render(){
+		return (
+			<button
+			onClick={this.setTimeout}>Delayed
+			</button>
+		)
+	}
+}
+
+
+export default DelayedButton
